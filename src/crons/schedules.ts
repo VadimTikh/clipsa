ad8337c1f94306dd39551f9e26df75c09e484778
VadimTikh <vadim.tikhonv@gmail.com>
2024-11-10 23:18:44 +0200

    erc cron every 4 hours

--- src/crons/schedules.ts
+++ src/crons/schedules.ts
@@ -2,7 +2,8 @@ import cron from 'node-cron';
 import {suppliers} from './handlers';
 import {log} from '../lib/log';
 
-cron.schedule('* * * * *', () => {
+// Every 4 hours
+cron.schedule('0 */4 * * *', () => {
   void suppliers.erc.parseContentProductsToDb();
   log('Cron job parseContentProductsToDb started');
 });
